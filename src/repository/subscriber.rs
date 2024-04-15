655fc854127300f5f0ee9976a98a9030a90b664f
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2024-04-15 19:21:56 +0700

    Implement list_all function in Subscriber repository.

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -21,5 +21,12 @@ impl SubscriberRepository {
         return subscriber;
     }
 
-    pub fn list_all(product_type: &str) -> Vec<Subscriber
+    pub fn list_all(product_type: &str) -> Vec<Subscriber> {
+        if SUBSCRIBERS.get(product_type).is_none() {
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        };
+
+        return SUBSCRIBERS.get(product_type).unwrap().iter()
+            .map(|f| f.value().clone()).collect();
+    }
 }
